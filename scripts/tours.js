792003a053ee47eaba28edf33ecd4872aa546508
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-11-30 00:06:00 +0100

    Creating temporary tour object

---
+++ scripts/tours.js
@@ -0,0 +1,49 @@
+
+
+// Get tour data from JSON
+(function() {
+	 //fetch('http://kallevikstyle.no/portfolio1/re-cycle/json/tours.json')
+	 //.then(result => result.json())
+	 //.then((tours) => {
+	 	// showAllProducts(tours);
+	 // })
+	 // .catch(err => console.log(err));
+
+
+// TEMPORARY-------------
+	 var tours = [
+  {
+    "name": "Discover Haugesund",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "shortdescription": "See the most noteworthy places of Haugesund in just one hour",
+    "imageUrl": "images/tour_discover-haugesund_580.jpg",
+    "thumbnail": "images/tour_discover-haugesund_375.jpg",
+    "id": 1
+  },
+  {
+    "name": "Hills &amp; Shore Tour",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "shortdescription": "Experience the rough shoreside and the nice view from the top of the hill",
+    "imageUrl": "images/tour_hills_580.jpg",
+    "thumbnail": "images/tour_hills_375.jpg",
+    "id": 2
+  },
+  {
+    "name": "Architec&hyphen;Tour",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "shortdescription": "Bicycle through the town history by visiting the most famous buildings",
+    "imageUrl": "images/tour_architectour_580.jpg",
+    "thumbnail": "images/tour_architectour_375.jpg",
+    "id": 3
+  },
+  {
+    "name": "Viking Trails",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "shortdescription": "Follow the trail of the old viking kings",
+    "imageUrl": "images/tour_viking-trails_580.jpg",
+    "thumbnail": "images/tour_viking-trails_375.jpg",
+    "id": 4
+  }
+];
+// TEMPORARY----------
+})();
